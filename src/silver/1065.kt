fdb83437cbe2f2c369225832af6d6ff095da583f
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-05 20:43:04 +0900

    commit

--- src/silver/1065.kt
+++ src/silver/1065.kt
@@ -5,8 +5,9 @@ fun main() {
     var count = 0
     val n = readLine()!!.toInt()
     for (i in 1..n) {
-        if (isHanSu(i))
+        if (isHanSu(i)) {
             count++
+        }
     }
     print(count)
 }
@@ -15,13 +16,14 @@ fun isHanSu(n: Int): Boolean {
     if (n < 100)
         return true
     else {
-        val array = n.toString().map { it.code - '0'.code }
+        val array = n.toString().map { it.code - '0'.code }.toMutableList()
         val gap = array[1] - array[0]
         var count = 0
-        for (i in array) {
-            if (i != array.last() && array[array.indexOf(i) + 1] - i == gap)
-                count++
+        while(array.size > 1){
+            if(array[1] - array[0] == gap)
+                array.removeFirst()
+            else return false
         }
-        return count == array.size - 1
+        return true
     }
 }
