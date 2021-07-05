9450ab6aeadd8ed4c0a949cd73a54937bf4e1054
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-05 19:23:04 +0900

    commit

--- src/silver/4673.kt
+++ src/silver/4673.kt
@@ -1,5 +1,16 @@
 package silver
 
 fun main() {
-
+    val list = ArrayList<Int>()
+    for (i in 1..10000)
+        list.add(i)
+    for (i in 1..10000) {
+        val nextN = nextNum(i)
+        if(nextN <= 10000 && list.contains(nextN))
+            list.remove(nextN)
+    }
+    for(i in list)
+        println(i)
 }
+
+fun nextNum(n: Int) = n + n.toString().map { it.code - '0'.code }.sum()
