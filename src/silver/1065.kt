360657677cf1077610bc23190c1a730888f5ab35
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-05 20:26:46 +0900

    commit

--- src/silver/1065.kt
+++ src/silver/1065.kt
@@ -4,22 +4,22 @@ package silver
 fun main() {
     var count = 0
     val n = readLine()!!.toInt()
-    for(i in 1 .. n){
-        if(isHanSu(i))
+    for (i in 1..n) {
+        if (isHanSu(i))
             count++
     }
     print(count)
 }
 
 fun isHanSu(n: Int): Boolean {
-    if(n < 100)
+    if (n < 100)
         return true
-    else{
+    else {
         val array = n.toString().map { it.code - '0'.code }
         val gap = array[1] - array[0]
         var count = 0
-        for(i in array){
-            if(i != array.last() && array[array.indexOf(i) + 1] - i == gap)
+        for (i in array) {
+            if (i != array.last() && array[array.indexOf(i) + 1] - i == gap)
                 count++
         }
         return count == array.size - 1
