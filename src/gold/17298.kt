3f00f3f6f0f8eb4fcaf1beecb37ade1300b44917
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 11:37:11 +0900

    commit

--- src/gold/17298.kt
+++ src/gold/17298.kt
@@ -6,7 +6,21 @@ fun main() {
     readLine()
     var arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
 
-    arr.map { one ->
+    val stack = ArrayList<Int>()
+    for(i in 0 until arr.size){
+        while(stack.isNotEmpty() && arr[stack.first()] < arr[i]){
+            arr[stack.first()] = arr[i]
+            stack.removeFirst()
+        }
+        stack.add(i)
+    }
+    stack.forEach {
+        arr[it] = -1
+    }
+    println(arr)
+
+
+    /*arr.map { one ->
         arr.find { arr.indexOf(it) > arr.indexOf(one) && it > one } ?: -1
-    }.toMutableList().forEach { print("$it ") }
+    }.toMutableList().forEach { print("$it ") }*/
 }
