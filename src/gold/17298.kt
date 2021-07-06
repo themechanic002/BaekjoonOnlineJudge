304d8cd7c7a8845de2bf582efdd32987d6b81b81
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 10:10:40 +0900

    commit

--- src/gold/17298.kt
+++ src/gold/17298.kt
@@ -4,15 +4,14 @@ package gold
 fun main() {
     val n = readLine()!!.toInt()
     val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
-    while(arr.isNotEmpty()){
+    while (arr.isNotEmpty()) {
         val target = arr.first()
         arr.removeFirst()
-        for(i in 0 .. arr.lastIndex){
-            if(arr[i] > target) {
+        for (i in 0..arr.lastIndex) {
+            if (arr[i] > target) {
                 print(arr[i])
                 break
-            }
-            else if(i == arr.lastIndex && arr[i] <= target){
+            } else if (i == arr.lastIndex && arr[i] <= target) {
                 print(-1)
                 break
             }
