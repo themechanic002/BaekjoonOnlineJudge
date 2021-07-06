16d22764b204a832f098b7f4c626e29414cb4e58
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 17:15:40 +0900

    commit

--- src/gold/1655.kt
+++ src/gold/1655.kt
@@ -1,17 +1,33 @@
 package gold
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 //가운데를 말해요
+/* 우선순위 큐 두 개를 생성해서
+* 한 개는 작은 수들로만 구성되어 있지만 peek()하면 제일 큰 수가 나오는 최대힙으로,
+* 한  개는 큰 수들로만 구성되어 있지만 peek()하면 제일 작은 수가 나오는 최소힙으로 해서
+* 중간 값을 찾아내는 방법 */
 fun main() {
-    val arr = ArrayList<Int>()
+    val pqLow = PriorityQueue<Int>(Collections.reverseOrder())
+    val pqHigh = PriorityQueue<Int>()
     val n = readLine()!!.toInt()
-    val answer = ArrayList<Int>()
+
     for (i in 0 until n) {
-        arr.add(readLine()!!.toInt())
-        arr.sort()
-        if (arr.size % 2 != 0)
-            answer.add(arr[arr.size / 2])
+        val next = readLine()!!.toInt()
+
+        if (pqLow.size == pqHigh.size)
+            pqLow.add(next)
         else
-            answer.add(arr[(arr.size / 2) - 1])
+            pqHigh.add(next)
+
+        if (pqLow.isNotEmpty() && pqHigh.isNotEmpty()) {
+            if (pqHigh.peek() < pqLow.peek()) {
+                val temp = pqHigh.poll()
+                pqHigh.add(pqLow.poll())
+                pqLow.add(temp)
+            }
+        }
+        println(pqLow.peek())
     }
-    answer.forEach { println(it) }
 }
