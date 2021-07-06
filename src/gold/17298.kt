8a5ece219abe7fcde6762438f4d71a1375e4ef54
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 10:41:07 +0900

    commit

--- src/gold/17298.kt
+++ src/gold/17298.kt
@@ -2,7 +2,7 @@ package gold
 
 //오큰수
 fun main() {
-    val n = readLine()!!.toInt()
+    readLine()
     val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
     while (arr.isNotEmpty()) {
         val target = arr.first()
