de2cef3d9168737adebe29bbb7628fb0e946ac2f
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 11:55:24 +0900

    commit

--- src/gold/17298.kt
+++ src/gold/17298.kt
@@ -5,12 +5,17 @@ import java.util.*
 //오큰수
 //시간초과에러
 fun main() {
-    readLine()
-    var arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    val n = readLine()!!.toInt()
+    val scanner = Scanner(System.`in`)
+    var arr = ArrayList<Int>()
+    for (i in 0 until n)
+        arr.add(scanner.nextInt())
+
+    //var arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
 
     val stack = Stack<Int>()
     for (i in 0 until arr.size) {
-        while (stack.isNotEmpty() && arr[stack.first()] < arr[i]) {
+        while (stack.isNotEmpty() && arr[stack.peek()] < arr[i]) {
             arr[stack.pop()] = arr[i]
         }
         stack.push(i)
