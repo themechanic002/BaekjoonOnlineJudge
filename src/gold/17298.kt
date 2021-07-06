304553cbd4b0d20eb4d3002ba7f0b1c020b842d9
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 10:10:31 +0900

    commit

---
+++ src/gold/17298.kt
@@ -0,0 +1,22 @@
+package gold
+
+//오큰수
+fun main() {
+    val n = readLine()!!.toInt()
+    val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    while(arr.isNotEmpty()){
+        val target = arr.first()
+        arr.removeFirst()
+        for(i in 0 .. arr.lastIndex){
+            if(arr[i] > target) {
+                print(arr[i])
+                break
+            }
+            else if(i == arr.lastIndex && arr[i] <= target){
+                print(-1)
+                break
+            }
+        }
+        print(" ")
+    }
+}
