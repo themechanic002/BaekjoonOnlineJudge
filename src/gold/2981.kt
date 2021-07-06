d8437ea126d2d2a5d403e9dfb693f3175662cc64
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 14:00:57 +0900

    commit

--- src/gold/2981.kt
+++ src/gold/2981.kt
@@ -1,16 +1,21 @@
 package gold
 
+//검문
+//에러
 fun main() {
     val n = readLine()!!.toInt()
     val arr = ArrayList<Int>()
     val answer = ArrayList<Int>()
     for (i in 0 until n)
-        arr.add(readLine()!!.toInt())
+        arr.add(i, readLine()!!.toInt())
 
-    val sortedArr = arr.sorted()
-    for (i in 2..sortedArr.last()) {
-        val temp = sortedArr.first() % i
-        if (sortedArr.all { it % i == temp })
+    val sortedArr = arr.sorted().toIntArray()
+    for (i in 0..sortedArr.first()) {
+        var newArr = sortedArr.copyOf()
+        newArr = newArr.map { if(it - i <= 0) 1 else it - i }.toIntArray()
+        newArr.forEach { print("$it ") }
+        println()
+        if(newArr.all { newArr.last() % it == 0 })
             answer.add(i)
     }
     answer.forEach { print("$it ") }
