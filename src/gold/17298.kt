18a67b8f5db5c0c0032e9b69d577c7e6b4a65c3e
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-06 10:18:07 +0900

    commit

--- src/gold/17298.kt
+++ src/gold/17298.kt
@@ -7,18 +7,11 @@ fun main() {
     while (arr.isNotEmpty()) {
         val target = arr.first()
         arr.removeFirst()
-        for (i in 0..arr.lastIndex) {
-            if (arr[i] > target) {
-                print(arr[i])
-                break
-            } else if (i == arr.lastIndex && arr[i] <= target) {
-                print("-1")
-                break
-            }
-        }
-        if (arr.isEmpty())
+        if(arr.all { it <= target })
             print(-1)
-        else
-            print(" ")
+        else {
+            print(arr.find { it > target })
+        }
+        print(" ")
     }
 }
