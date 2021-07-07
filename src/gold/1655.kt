eb485654e07cb204fbafd5204a7d6cda9e918697
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-07 13:00:52 +0900

    commit

--- src/gold/1655.kt
+++ src/gold/1655.kt
@@ -1,5 +1,9 @@
 package gold
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.util.*
 
 //가운데를 말해요
@@ -8,27 +12,32 @@ import java.util.*
 * 한  개는 큰 수들로만 구성되어 있지만 peek()하면 제일 작은 수가 나오는 최소힙으로 해서
 * 중간 값을 찾아내는 방법 */
 
-//코틀린으로만 오류남
+//입출력을 readLine과 println으로 하면 안 됨. 시간을 줄이기 위해 BufferedReader와 BufferedWriter를 사용해야함.
+
 fun main() {
     val pqLow = PriorityQueue<Int>(Collections.reverseOrder())
     val pqHigh = PriorityQueue<Int>()
-    val n = readLine()!!.toInt()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = Integer.parseInt(br.readLine())
 
     for (i in 0 until n) {
-        val next = readLine()!!.toInt()
+        val next = Integer.parseInt(br.readLine())
 
         if (pqLow.size == pqHigh.size)
-            pqLow.add(next)
+            pqLow.offer(next)
         else
-            pqHigh.add(next)
+            pqHigh.offer(next)
 
         if (pqLow.isNotEmpty() && pqHigh.isNotEmpty()) {
             if (pqHigh.peek() < pqLow.peek()) {
                 val temp = pqHigh.poll()
-                pqHigh.add(pqLow.poll())
-                pqLow.add(temp)
+                pqHigh.offer(pqLow.poll())
+                pqLow.offer(temp)
             }
         }
-        println(pqLow.peek())
+        bw.write("${pqLow.peek()}\n")
     }
+    br.close()
+    bw.close()
 }
