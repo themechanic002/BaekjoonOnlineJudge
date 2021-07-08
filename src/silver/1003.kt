acd85d8ad77739d678e341ebf2e7264dd58b995c
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-08 22:43:59 +0900

    commit

--- src/silver/1003.kt
+++ src/silver/1003.kt
@@ -5,19 +5,17 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-var countZero = 0
-var countOne = 0
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val n = Integer.parseInt(br.readLine())
     for (i in 0 until n) {
-        countZero = 0
-        countOne = 0
-        fib(Integer.parseInt(br.readLine()))
-        bw.write("$countZero $countOne\n")
+        val next = Integer.parseInt(br.readLine())
+        if (next == 0)
+            bw.write("1 0\n")
+        else
+            bw.write("${fib(next - 1)} ${fib(next)}\n")
     }
     br.close()
     bw.close()
@@ -25,10 +23,8 @@ fun main() {
 
 fun fib(n: Int): Int {
     if (n == 0) {
-        countZero++
         return 0
     } else if (n == 1) {
-        countOne++
         return 1
     } else return fib(n - 1) + fib(n - 2)
 }
