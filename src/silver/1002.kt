2e897daee789963dbc98f1146624c4835fa64d95
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-09 10:10:32 +0900

    commit

--- src/silver/1002.kt
+++ src/silver/1002.kt
@@ -13,13 +13,13 @@ fun main() {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val n = Integer.parseInt(br.readLine())
-    for(i in 0 until n){
+    for (i in 0 until n) {
         val line = br.readLine().split(" ").map { it.toInt() }
-        val xToy = sqrt(abs(line[0] - line[3]).toDouble() + abs(line[1] - line [4]))
+        val xToy = sqrt(abs(line[0] - line[3]).toDouble() + abs(line[1] - line[4]))
         val r1Plusr2 = (line[2] + line[5]).toDouble()
-        if(xToy > r1Plusr2)
+        if (xToy > r1Plusr2)
             bw.write("0\n")
-        else if(xToy < r1Plusr2)
+        else if (xToy < r1Plusr2)
             bw.write("2\n")
         else
             bw.write("1\n")
