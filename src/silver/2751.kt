31bcb2f08c7d5e3bead542608a2d833f2b8cc216
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-09 13:33:47 +0900

    commit

---
+++ src/silver/2751.kt
@@ -0,0 +1,10 @@
+package silver
+
+//수 정렬하기 2
+fun main() {
+    val n = readLine()!!.toInt()
+    val arr = ArrayList<Int>()
+    for(i in 0 until n)
+        arr.add(readLine()!!.toInt())
+    arr.sorted().forEach { println(it) }
+}
