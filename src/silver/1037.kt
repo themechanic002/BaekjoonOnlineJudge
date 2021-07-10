091599ee040dc94ff745005328cbc414aafedc48
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-10 19:33:32 +0900

    commit

--- src/silver/1037.kt
+++ src/silver/1037.kt
@@ -7,7 +7,10 @@ import java.io.OutputStreamWriter
 
 //약수
 fun main() {
-
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = Integer.parseInt(br.readLine())
+    val divisors = br.readLine().toString().split(" ").map { it.toInt() }.sorted()
+    bw.write(divisors.first() * divisors.last())
 }
