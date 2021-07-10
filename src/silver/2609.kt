fc545e66c540c686320e82fd1c7961c50a55e0d9
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-10 19:45:55 +0900

    commit

---
+++ src/silver/2609.kt
@@ -0,0 +1,26 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//최대공약수와 최소공배수
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val arr = br.readLine().split(" ").map { it.toInt() }.sortedDescending()
+    val gcdVal = gcd(arr[0], arr[1])
+    bw.write("${gcdVal}\n")
+    bw.write("${arr[0] * arr[1] / gcdVal}")
+}
+fun gcd(a: Int, b: Int): Int{
+    var a2 = a
+    var b2 = b
+    while (b2 !== 0) {
+        val r: Int = a2 % b2
+        a2 = b2
+        b2 = r
+    }
+    return a2
+}
