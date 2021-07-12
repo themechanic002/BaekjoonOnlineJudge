cc8d6cdf0ecd4c047770fe9b0d9aaf92d72d12e0
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-12 18:38:12 +0900

    commit

---
+++ src/silver/9012.kt
@@ -0,0 +1,12 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//괄호
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+}
