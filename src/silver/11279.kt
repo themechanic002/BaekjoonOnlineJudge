4b971840c04c442f189b46b6f37898ab2eee3657
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-12 18:51:34 +0900

    commit

--- src/silver/11279.kt
+++ src/silver/11279.kt
@@ -4,10 +4,22 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 //최대 힙
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-
+    val pq = PriorityQueue<Int>()
+    val n = Integer.parseInt(br.readLine())
+    for(i in 0 until n){
+        val next = Integer.parseInt(br.readLine())
+        if(next == 0){
+            if(pq.isEmpty())
+                bw.write("0\n")
+            else
+                bw.write("${pq.poll()}\n")
+        }
+        else pq.offer(next)
+    }
 }
