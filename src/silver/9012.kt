83d27e55bc78ed905a436801b93f8074ee2d2584
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-12 18:43:29 +0900

    commit

--- src/silver/9012.kt
+++ src/silver/9012.kt
@@ -4,9 +4,33 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 //괄호
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = Integer.parseInt(br.readLine())
+    for(i in 0 until n){
+        val line = br.readLine()
+        if(isRight(line))
+            bw.write("YES\n")
+        else bw.write("NO\n")
+    }
+    bw.close()
+}
+fun isRight(s: String): Boolean{
+    val stack = Stack<Char>()
+    for(i in s){
+        when(i){
+            '(' -> stack.push('(')
+            ')' -> {
+                if(stack.isEmpty())
+                    return false
+                else
+                    stack.pop()
+            }
+        }
+    }
+    return stack.isEmpty()
 }
