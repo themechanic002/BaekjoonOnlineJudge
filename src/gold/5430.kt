8e94eac9b6a1b923e12edaca49da7d5608a525fb
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-15 09:50:36 +0900

    commit

--- src/gold/5430.kt
+++ src/gold/5430.kt
@@ -11,11 +11,35 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = Integer.parseInt(br.readLine())
-    for(i in 0 until n){
+    first@for(i in 0 until n){
         val p = br.readLine().toString().toCharArray()
         val arrN = Integer.parseInt(br.readLine())
         val arr = ArrayDeque<Int>()
+        var isError = false
         br.readLine().toString().substring(1, arrN * 2).split(",").map { it.toInt() }.forEach { arr.add(it) }
-
+        for(i in 0 until p.size){
+            when(p[i]){
+                'R'->{
+                    if(!arr.isEmpty()){
+                        val newArr = ArrayDeque<Int>()
+                        while(arr.isNotEmpty())
+                            newArr.add(arr.removeLast())
+                        arr.addAll(newArr)
+                    }
+                }
+                'D'->{
+                    if(arr.isEmpty()) {
+                        isError = true
+                        break
+                    }
+                    else{
+                        arr.removeFirst()
+                    }
+                }
+            }
+        }
+        if(isError) bw.write("error\n")
+        else bw.write("${arr}\n")
     }
+    bw.close()
 }
