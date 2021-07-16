25e53e426f106e687069bdc51e3639bb2d3f8c71
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-16 11:21:07 +0900

    commit

--- src/silver/1966.kt
+++ src/silver/1966.kt
@@ -4,10 +4,22 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 //프린터 큐
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val testNum = Integer.parseInt(br.readLine())
+    for(i in 0 until testNum){
+        val tempArr = br.readLine().toString().split(" ").map { it.toInt() }
+        val n = tempArr[0]
+        val m = tempArr[1]
+        val queue = LinkedList<Int>()
+        br.readLine().toString().split(" ").map { it.toInt() }.forEach { queue.offer(it) }
 
+
+
+
+    }
 }
