0a9c6287dfbbd700f6769a3e39647282e2627bce
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-16 11:14:04 +0900

    commit

--- src/gold/11444.kt
+++ src/gold/11444.kt
@@ -1,22 +1,28 @@
 package gold
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 //피보나치 수 6
 //행렬을 이용해야하지만 좀 더 고민이 필요할 거 같음.
 //미완성
 fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     val c = 1000000007
-    val n = readLine()!!.toLong() % c.toLong()
-    val fib = ArrayList<Long>()
+    val n = br.readLine().toLong()
+    val fib = ArrayList<Int>()
     for (i in 0L..n) {
         if (fib.size == 0)
             fib.add(0)
         else if (fib.size == 1)
             fib.add(1)
         else {
-            fib.add(((fib.last() % c) + (fib[fib.lastIndex - 1] % c)) % c)
+            fib.add((fib.last() + fib[fib.lastIndex - 1]) % c)
             fib.removeFirst()
         }
     }
-    print(fib.last())
-
+    bw.write("${fib.last()}\n")
 }
---
+++ src/silver/1966.kt
@@ -0,0 +1,13 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//프린터 큐
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+}
