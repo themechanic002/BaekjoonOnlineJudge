32e20cff445c65fc97cda1251aa5de552d0d592d
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-17 11:10:56 +0900

    commit

--- src/silver/18258.kt
+++ src/silver/18258.kt
@@ -4,14 +4,22 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import kotlin.collections.ArrayDeque
 
 //큐2
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = Integer.parseInt(br.readLine())
+    val dq = ArrayDeque<Int>()
     for(i in 0 until n){
         val order = br.readLine().toString()
-
+        if(order.contains("push"))
+            dq.addLast(order.replace("push ", "").toInt())
+        else{
+            when(order){
+                "front" -> bw.write("${dq}")
+            }
+        }
     }
 }
