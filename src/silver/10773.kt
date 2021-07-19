78cd7d31b774ceb961880d5ea75150c4fa109c6d
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-19 21:59:44 +0900

    commit

--- src/silver/10773.kt
+++ src/silver/10773.kt
@@ -4,13 +4,21 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 //제로
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = Integer.parseInt(br.readLine())
+    val stack = Stack<Int>()
     for(i in 0 until n){
-
+        val num = Integer.parseInt(br.readLine())
+        when(num){
+            0 -> stack.pop()
+            else -> stack.add(num)
+        }
     }
+    bw.write(stack.sum())
+    bw.close()
 }
