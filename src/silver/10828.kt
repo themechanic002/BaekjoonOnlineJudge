05ccba4335365ed8b3cd14c0326eb2898d86e3de
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-20 12:45:18 +0900

    commit

--- src/silver/10828.kt
+++ src/silver/10828.kt
@@ -11,23 +11,23 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val stack = Stack<Int>()
-    for(i in 0 until Integer.parseInt(br.readLine())){
+    for (i in 0 until Integer.parseInt(br.readLine())) {
         val order = br.readLine().toString()
-        if(order.contains("push "))
+        if (order.contains("push "))
             stack.add(order.replace("push ", "").toInt())
-        else{
-            when(order){
+        else {
+            when (order) {
                 "pop" -> {
-                    if(stack.isEmpty()) bw.write("-1\n")
+                    if (stack.isEmpty()) bw.write("-1\n")
                     else bw.write("${stack.pop()}\n")
                 }
                 "size" -> bw.write("${stack.size}\n")
                 "empty" -> {
-                    if(stack.isEmpty()) bw.write("1\n")
+                    if (stack.isEmpty()) bw.write("1\n")
                     else bw.write("0\n")
                 }
                 "top" -> {
-                    if(stack.isEmpty()) bw.write("-1\n")
+                    if (stack.isEmpty()) bw.write("-1\n")
                     else bw.write("${stack.peek()}\n")
                 }
             }
