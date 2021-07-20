866cd5d75ac5a9a43b542c96291dc44d25f3ccfe
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-21 08:54:38 +0900

    commit

---
+++ src/silver/11866.kt
@@ -0,0 +1,23 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//요세푸스 문제 0
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val temp = br.readLine().toString().split(" ").map { it.toInt() }
+    val n = temp[0]
+    val k = temp[1]
+    val dq = ArrayDeque<Int>()
+    for(i in 1 .. n)
+        dq.add(i)
+    while(dq.isNotEmpty()){
+        for(i in 0 until k)
+            dq.addLast(dq.removeFirst())
+        dq.removeFirst()
+    }
+}
