882524d30e5c073ccf05a0bce4b24b47ccdf8b6a
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-22 10:40:31 +0900

    commit

--- src/silver/10866.kt
+++ src/silver/10866.kt
@@ -9,5 +9,16 @@ import java.io.OutputStreamWriter
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = Integer.parseInt(br.readLine())
+    val dq = ArrayDeque<Int>()
+    for(i in 0 until n){
+        val order = br.readLine().toString()
+        if(order.contains("push_back "))
+            dq.addLast(order.replace("push_back ", "").toInt())
+        else if(order.contains("push_front "))
+            dq.addLast(order.replace("push_front ", "").toInt())
+        else{
 
+        }
+    }
 }
