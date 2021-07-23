3118b40a4ada3b7f8213f61e6fc6f1849fd06244
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-23 10:38:45 +0900

    commit

--- src/silver/4948.kt
+++ src/silver/4948.kt
@@ -9,23 +9,25 @@ import java.io.OutputStreamWriter
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val prime: Array<Boolean> = Array(123456) { true }
+    prime[0] = false
+    prime[1] = false
+    for (i in 2..123456) {
+        if (prime[i])
+            for (j in 2 * i..1000 step i) prime[i] = false
+    }
+
     while (true) {
         val next = Integer.parseInt(br.readLine())
         if (next == 0)
             break
         else {
             var count = 0
-            for (i in next + 1..next * 2) {
-                var prime = true
-                for (j in 2..next) {
-                    if (i % j == 0) {
-                        prime = false
-                        break
-                    }
-                }
-                if (prime) count++
-            }
-            bw.write("${count}\n")
+            for (i in next + 1..next * 2)
+                if (prime[i])
+                    count++
+            bw.write("$count\n")
         }
     }
     bw.close()
