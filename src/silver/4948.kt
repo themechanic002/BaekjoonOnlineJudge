592e955eb88d1aee78f7528847b90a4ca5af3eef
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-23 10:27:03 +0900

    commit

---
+++ src/silver/4948.kt
@@ -0,0 +1,33 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//베르트랑 공준
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    while(true){
+        var count = 0
+        val next = Integer.parseInt(br.readLine())
+        if(next == 0)
+            break
+        else{
+            val arr = ArrayList<Int>()
+            for(i in next + 1 .. next * 2) {
+                var prime = true
+                for(j in 2 .. next){
+                    if(i % j == 0){
+                        prime = false
+                        break
+                    }
+                }
+                if(prime) arr.add(i)
+            }
+            bw.write("${arr.size}\n")
+        }
+    }
+    bw.close()
+}
