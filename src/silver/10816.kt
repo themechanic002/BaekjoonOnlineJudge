58d21aee1e9122dbaa10cb7bab65c6862f25c922
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-25 22:29:31 +0900

    commit

---
+++ src/silver/10816.kt
@@ -0,0 +1,18 @@
+package silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+//숫자 카드 2
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = Integer.parseInt(br.readLine())
+    val map = HashMap<Int, Int>()
+    br.readLine().toString().split(" ").map { it.toInt() }.forEach {
+
+    }
+
+}
