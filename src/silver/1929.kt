800b79f4a6095b4b03c2332f42a8aedf08532896
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-26 08:48:01 +0900

    commit

--- src/silver/1929.kt
+++ src/silver/1929.kt
@@ -12,10 +12,14 @@ fun main() {
     val m = line[0]
     val n = line[1]
     val arr : Array<Boolean> = Array(n + 1) {true}
+    arr[1] = false
     for(i in 2 .. n){
         if(arr[i])
             for(j in i * 2 .. n step i)
                 arr[j] = false
     }
-    
+    for(i in m .. n)
+        if(arr[i])
+            bw.write("$i\n")
+    bw.close()
 }
