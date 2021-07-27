2b31ca2d35f9ad2f642bf40f01c77ce023726e40
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-27 09:43:18 +0900

    commit

--- src/silver/2108.kt
+++ src/silver/2108.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import kotlin.math.roundToInt
 
 //통계학
 fun main() {
@@ -15,17 +16,9 @@ fun main() {
         arr.add(Integer.parseInt(br.readLine()))
     }
     arr.sort()
-    val avg = arr.average()
-    if(avg >= avg.toInt() + 0.5)
-        if(avg < 0)
-            bw.write("${avg.toInt() - 1}\n")
-        else
-            bw.write("${avg.toInt() + 1}\n")
-    else
-        bw.write("${avg.toInt()}\n")
-
+    bw.write("${arr.average().roundToInt()}\n")
     bw.write("${arr[n / 2]}\n")
-    bw.write("${arr.groupBy { it }}\n")
+    bw.write("${arr.groupBy { it }.values.sortedWith(compareBy({-it.size}, {it.first()})).get(1).first()}\n")
     bw.write("${arr.last() - arr.first()}")
     bw.close()
 }
