b142cdef46d6dcb04d65163ee90be2c84ea4ff0f
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-27 09:23:13 +0900

    commit

--- src/silver/2108.kt
+++ src/silver/2108.kt
@@ -10,8 +10,20 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = Integer.parseInt(br.readLine())
-    val arr = ArrayList<Int>()
-    for(i in 0 until n){
-
+    val arr = ArrayList<Pair<Int, Int>>()
+    var sum = 0
+    for(i in 0 until n) {
+        val input = Integer.parseInt(br.readLine())
+        sum += input
+        arr.add(Pair(input, 1))
     }
+    arr.sortBy { it.first }
+    bw.write("${sum / n}")
+    bw.write("${arr[n/2].first}")
+    val max = arr[n - 1].first
+    val min = arr[0].first
+    arr.sortWith(compareBy({-it.second}, { it.first }))
+    bw.write("${arr[arr[0].second].first}")
+    bw.write("${max - min}")
+    bw.close()
 }
