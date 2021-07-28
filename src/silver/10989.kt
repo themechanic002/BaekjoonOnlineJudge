1dc9a0ef60f30c50ed0d41c2981ea7e272e2aa5b
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-28 09:27:27 +0900

    commit

--- src/silver/10989.kt
+++ src/silver/10989.kt
@@ -4,14 +4,16 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 //수 정렬하기 3
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val arr = ArrayList<Int>()
+    val arr = PriorityQueue<Int>(Comparator.reverseOrder())
     for (i in 0 until Integer.parseInt(br.readLine()))
-        arr.add(Integer.parseInt(br.readLine()))
-    arr.sorted().forEach { bw.write("$it\n") }
+        arr.offer(Integer.parseInt(br.readLine()))
+    while (arr.isNotEmpty())
+        bw.write("${arr.poll()}")
     bw.close()
 }
