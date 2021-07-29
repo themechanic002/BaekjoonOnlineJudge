389357d189973990a0d22b91fee792f1c79327f7
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-29 13:51:22 +0900

    commit

--- src/silver/18870.kt
+++ src/silver/18870.kt
@@ -9,4 +9,11 @@ import java.io.OutputStreamWriter
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val n = Integer.parseInt(br.readLine())
+    val set = HashSet<Pair<Int, Int>>()
+    val arr = IntArray(n)
+    val tmp = br.readLine().split(" ")
+    for(i in 0 until n)
+        arr[i] = tmp[i].toInt()
+
 }
