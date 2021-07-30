79518a204e5383b167667761d5f5c8709a99e158
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-30 13:09:30 +0900

    commit

--- src/silver/3036.kt
+++ src/silver/3036.kt
@@ -12,15 +12,10 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     br.readLine()
-    val arr = ArrayList<Int>()
     val line = br.readLine().split(" ").map { it.toInt() }
     val standard = line[0]
-    line.forEach {
-        if (line.indexOf(it) > 0)
-            arr.add(it)
-    }
-    arr.forEach {
-        var a = it
+    for (i in 1..line.lastIndex) {
+        var a = line[i]
         var b = standard
         if (a == b)
             bw.write("1/1\n")
@@ -40,7 +35,6 @@ fun main() {
 fun findingGCD(a: Int, b: Int): Int {
     val maximum = max(a, b)
     val minimum = min(a, b)
-
     return if (minimum == 0) max(a, b)
     else findingGCD(minimum, maximum % minimum)
 }
