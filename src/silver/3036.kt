7a5cef8955e50b450776da5c53c4fcc4ce3a2f74
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-30 12:57:12 +0900

    commit

--- src/silver/3036.kt
+++ src/silver/3036.kt
@@ -4,6 +4,8 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.lang.Math.max
+import java.lang.Math.min
 
 //링
 fun main() {
@@ -17,7 +19,25 @@ fun main() {
         if(line.indexOf(it) > 0)
             arr.add(it)
     }
+    arr.forEach {
+        var a = it
+        var b = standard
+        var gcd = findingGCD(a, b)
+        while(gcd != 1){
+            a /= gcd
+            b /= gcd
+            gcd = findingGCD(a, b)
+        }
+        bw.write("$b/$a\n")
+    }
+    bw.close()
 
+}
 
+fun findingGCD(a: Int, b: Int): Int {
+    var maximum = max(a, b)
+    var minimum = min(a, b)
 
+    if (minimum == 0) return max(a, b)
+    else return findingGCD(minimum, maximum % minimum)
 }
