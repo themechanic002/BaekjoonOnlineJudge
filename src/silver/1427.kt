8283240ddf2769426ec7d1c9da981b792da0fdd0
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-07-30 12:37:19 +0900

    commit

--- src/silver/1427.kt
+++ src/silver/1427.kt
@@ -2,5 +2,5 @@ package silver
 
 //소트인사이드
 fun main() {
-    println("${readLine()?.toCharArray()?.sortedDescending().toString()}")
+    println(readLine()?.toCharArray()?.sortedDescending().toString())
 }
