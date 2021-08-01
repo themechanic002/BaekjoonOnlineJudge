6c1d55c9363e51e09990d1ef25ae53f0cf383fd8
안병현 Ahn ByeongHyeon <themechanic002@gmail.com>
2021-08-01 19:12:20 +0900

    commit

--- src/silver/11651.kt
+++ src/silver/11651.kt
@@ -13,6 +13,6 @@ fun main() {
     val arr = ArrayList<List<Int>>()
     for (i in 0 until n)
         arr.add(br.readLine().split(" ").map { it.toInt() })
-    arr.sortedWith(compareBy({ it[1] }, { it[0] })).forEach { bw.write("${it[0]} ${it[1]}") }
+    arr.sortedWith(compareBy({ it[1] }, { it[0] })).forEach { bw.write("${it[0]} ${it[1]}\n") }
     bw.close()
 }
